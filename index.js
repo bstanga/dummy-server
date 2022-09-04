d73fe5ebf46f31207b0ece09500ed5f8641294c3
Bogdan Stanga <bogydaci@gmail.com>
2022-09-04 10:24:49 +0200

    update port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
