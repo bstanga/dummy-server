39dba6c89fe9fd76de47a4f8cb6ade0388cfa629
Bogdan Stanga <bogydaci@gmail.com>
2022-12-08 16:47:35 +0200

    zdep

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get("/a", (req, res) => {
-  res.send("hellov5");
+app.get("/", (req, res) => {
+  res.send("hellov6");
 });
 
 app.get("/json", (req, res) => {
