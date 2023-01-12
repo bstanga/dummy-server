5f987a15ef915e1aa2f3f97ab251dde8b05b2106
Bogdan Stanga <bogydaci@gmail.com>
2023-01-12 19:48:44 +0100

    no response on /

--- app/index.js
+++ app/index.js
@@ -3,13 +3,13 @@ const app = express();
 const port = 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get("/", (req, res) => {
-  res.json({
-    time: new Date(),
-    environemnt: "main",
-    note: "deployment by malpe.io",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.json({
+//     time: new Date(),
+//     environemnt: "main",
+//     note: "deployment by malpe.io",
+//   });
+// });
 
 app.get("/json", (req, res) => {
   res.json({ now: new Date() });
